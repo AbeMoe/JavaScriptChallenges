7e6b68ead8d8934be4fbf7ee7c917e35e1d719cb
AbeMoe <40703430+AbeMoe@users.noreply.github.com>
2019-08-21 16:23:37 -0700

    Largest Number -- Organized array into a dictionary

--- Largest Number/script.js
+++ Largest Number/script.js
@@ -3,17 +3,27 @@
 //         into their largest possible configuration
 //         Note: returns them as a string to avoid overflow
 //         (so it's basically a sort)
-const nums = [22,23,12,15,105]
+const nums = [22,23,12,15,105,999]
 var obj = {};
-for(var number in nums)
+//Throw everything into our dictionary
+for(var number of nums)
 {
-  for(var digit in number)
+  number = number.toString(); //Make it into a string to make it enumerable
+  console.log(number);
+  for(var digit of number)
   {
+    console.log(digit);
     if(obj[digit] === undefined)
     {
-      obj[digit] = 0;
+      obj[digit] = 1;
     }
+    else
+      obj[digit] +=1;
   }
+}
+for (var i=0; i <10; i++)
+{
+  console.log(i);
 }
 
 console.log(obj)
