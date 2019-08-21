5460cf8c184f5a22fb783f079aaaf33617125454
AbeMoe <40703430+AbeMoe@users.noreply.github.com>
2019-08-21 14:00:39 -0700

    Added testing code for verifying answers.

--- multiplyStrings/script.js
+++ multiplyStrings/script.js
@@ -2,25 +2,30 @@
 //Purpose: multiplies two strings by their numeric value
 //         and returns the product
 //Restraints: cannot simply convert them to integers.
-num1="200";
+num1="20000";
+numberfied1 = Number(num1) // for testing
 num1 = num1.split('').reverse().join('')
-num2="350";
+num2= "55";
+numberfied2 = Number(num2) // for testing
+
 num2 = num2.split('').reverse().join('')
 console.log(num2);
-basePow=0;
-movingPow=0;
+num1Pow=0;
+num2Pow=0;
 total=0;
 for(int1 of num1)
 {
 
-  tempInt1 = int1.charCodeAt(0)-48;
+  tempInt1 = (int1.charCodeAt(0)-48) * Math.pow(10,num1Pow);
+  num2Pow=0;
   for(int2 of num2)
   {
-    movingPow=basePow;
-    tempInt2 = int2.charCodeAt(0)-48;
-    total += (tempInt1 * tempInt2) * (Math.pow(10,movingPow))
-    movingPow+=1;
+    tempInt2 = (int2.charCodeAt(0)-48) * Math.pow(10,num2Pow);
+    console.log(tempInt1*tempInt2)
+    total += tempInt1*tempInt2
+    num2Pow+=1;
   }
-  basePow+=1;
+  num1Pow+=1;
 }
+    console.log("expected answer is", numberfied1 * numberfied2)
     console.log(total);
