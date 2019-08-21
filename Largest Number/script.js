15cdc715630915d4702b946d2b7c046d224ea0f2
AbeMoe <40703430+AbeMoe@users.noreply.github.com>
2019-08-21 16:40:58 -0700

    Misunderstood the question. Making a new branch (may just put stuff in separate repositories for easier book keeping

--- Largest Number/script.js
+++ Largest Number/script.js
@@ -3,13 +3,12 @@
 //         into their largest possible configuration
 //         Note: returns them as a string to avoid overflow
 //         (so it's basically a sort)
-const nums = [22,23,12,15,105,999]
+const nums = [3,30,34,5,9]
 var obj = {};
 //Throw everything into our dictionary
 for(var number of nums)
 {
   number = number.toString(); //Make it into a string to make it enumerable
-  console.log(number);
   for(var digit of number)
   {
     console.log(digit);
@@ -21,9 +20,17 @@ for(var number of nums)
       obj[digit] +=1;
   }
 }
-for (var i=0; i <10; i++)
+returnString = "";
+for(var i=9;i>=0;i--)
 {
-  console.log(i);
+  if(obj[i] != undefined)
+  {
+    console.log(i)
+    while(obj[i] > 0)
+    {
+      returnString+=i;
+      obj[i]-=1;
+    }
+  }
 }
-
-console.log(obj)
+console.log(returnString);
